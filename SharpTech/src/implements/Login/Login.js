9bdb9c587c5a85dee645420dce5f954c5aa91cca
unknown <sharptechgayathri@gmail.com>
2024-06-27 10:02:26 +0530

    Gayathri

--- SharpTech/src/implements/Login/Login.js
+++ SharpTech/src/implements/Login/Login.js
@@ -1,39 +1,31 @@
-import React, { useState, useContext } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import LoginNavbar from '../LoginNavbar/LoginNavbar';
 import Footer from '../../components/Footer/Footer';
-import AuthContext from '../AuthContext/AuthContext';
 import UserService from '../UserService/UserService';
-import L_1 from '../../assets/login_image_6.jpg'
-import { MdEmail } from 'react-icons/md'; // Importing MdEmail icon
-import { FaLinkedin, FaInstagram, FaEnvelope, } from 'react-icons/fa';
-import { FaFacebook } from 'react-icons/fa';
+import L_1 from '../../assets/login_image_6.jpg';
 import { Link } from "react-router-dom";
+import { ProgressSpinner } from 'primereact/progressspinner'; // Importing ProgressSpinner
 import "./Login.css";
 
 const Login = () => {
   const [email, setEmail] = useState('');
-  // const [transactionid, setTansactionId] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  // const { login } = useContext(AuthContext);
+  const [loading, setLoading] = useState(false); // Add loading state
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true); // Set loading to true when login process starts
 
     try {
       const response = await UserService.login(email, password);
-       console.log(response);
-      if (response.statusCode==200) {
-        // login(userData.token, userData.role, userData.transactionId);
+      console.log(response);
+      if (response.statusCode === 200) {
         localStorage.setItem('email', email); // Store email in localStorage
         alert(response.message);
-
-
-        // localStorage.setTansactionId('transactionid',userData.transactionId);     // Storing the transaction ID in local Storage
-        // console.log(transactionid);
-        navigate('/VerifyLogin',{ state: { email } });  // Pass email in state
+        navigate('/VerifyLogin', { state: { email } });  // Pass email in state
       } else {
         setError(response.message);
         setTimeout(() => {
@@ -45,6 +37,8 @@ const Login = () => {
       setTimeout(() => {
         setError('');
       }, 5000);
+    } finally {
+      setLoading(false); // Set loading to false when login process completes
     }
   };
 
@@ -52,41 +46,39 @@ const Login = () => {
     <div>
       <LoginNavbar />
       <div className='login-page-container'>
-
         <div className='login-image'>
           <img src={L_1} alt="" />
         </div>
-
         <form className='login-form' onSubmit={handleSubmit}>
-
           <div className='login-heading-container'>
             <h1>Welcome to Sharp Tech Systems</h1> <br />
-            <p>Please login with below details to access your account. </p>
+            <p>Please login with below details to access your account.</p>
           </div>
           {error && <p className="error-message">{error}</p>}
-
           <div className='input-group'>
-            <label >Email</label>
+            <label>Email</label>
             <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
           </div>
-
           <div className='input-group'>
-            <label >Password</label>
+            <label>Password</label>
             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
           </div>
-
-          <button className='login-button' type="submit">Login</button>
-
+          <button className='login-button' type="submit" disabled={loading}>
+            {loading ? (
+              <>
+                <ProgressSpinner style={{ width: '24px', height: '18px', marginRight: '8px' }} strokeWidth="4" />
+                Logging in...
+              </>
+            ) : (
+              'Login'
+            )}
+          </button>
           <br />
-
           <Link to="/ForgetPassword" className='forgot-password'>Forgot Password?</Link>
-       
         </form>
       </div>
-      <Footer/>
+      <Footer />
     </div>
-
-
   );
 };
 
--- SharpTech/src/implements/Login/VerifyLogin.js
+++ SharpTech/src/implements/Login/VerifyLogin.js
@@ -1,73 +1,77 @@
 import React, { useState, useContext } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import axios from 'axios';
+import { ProgressSpinner } from 'primereact/progressspinner'; // Importing ProgressSpinner
 import UserService from '../UserService/UserService';
 import AuthContext from '../AuthContext/AuthContext';
 import './VerifyLogin.css';
 
 const VerifyLogin = () => {
+  const [otp, setOtp] = useState('');
+  const [message, setMessage] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false); // Add loading state
+  const navigate = useNavigate(); // Using useNavigate hook
 
+  const { verifylogin } = useContext(AuthContext);
+  const location = useLocation();
+  const { email } = location.state || {}; // Get the email from state
 
-    const [otp, setOtp] = useState('');
-    // const [newPassword, setNewPassword] = useState('');
-    const [message, setMessage] = useState('');
-    const [transactionid, setTansactionId] = useState('');
-    const [error, setError] = useState('');
-    const navigate = useNavigate(); // Using useNavigate hook
+  if (!email) {
+    return <p>No email provided. Please go back and enter your email again.</p>;
+  }
 
-    const { verifylogin } = useContext(AuthContext);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true); // Set loading to true when verification process starts
 
-    const location = useLocation();
-    const { email } = location.state || {};  // Get the email from state
-  
-    if (!email) {
-      return <p>No email provided. Please go back and enter your email again.</p>;
+    try {
+      const userData = await UserService.verifylogin(email, otp);
+      console.log(userData);
+      if (userData.token) {
+        verifylogin(userData.token, userData.role, userData.transactionId);
+        localStorage.setItem('email', email); // Store email in localStorage
+        navigate('/DisplayLogin');
+      } else {
+        setError(userData.message);
+        setTimeout(() => {
+          setError('');
+        }, 5000);
+      }
+    } catch (error) {
+      setError(error.message);
+      setTimeout(() => {
+        setError('');
+      }, 5000);
+    } finally {
+      setLoading(false); // Set loading to false when verification process completes
     }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-    
-        try {
-          const userData = await UserService.verifylogin(email, otp);
-           console.log(userData);
-          if (userData.token) {
-            verifylogin(userData.token, userData.role, userData.transactionId);
-            localStorage.setItem('email', email); // Store email in localStorage
-    
-    
-            // localStorage.setTansactionId('transactionid',userData.transactionId);     // Storing the transaction ID in local Storage
-            console.log(transactionid);
-            navigate('/DisplayLogin');
-          } else {
-            setError(userData.message);
-            setTimeout(() => {
-              setError('');
-            }, 5000);
-          }
-        } catch (error) {
-          setError(error.message);
-          setTimeout(() => {
-            setError('');
-          }, 5000);
-        }
-      };
+  return (
+    <div className="verify-login-container">
+      <div className="verify-login-box">
+        <h1>Enter OTP</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="verify-login-input-group">
+            <label>OTP:</label>
+            <input type="text" value={otp} onChange={(e) => setOtp(e.target.value)} required />
+          </div>
+          <button type="submit" disabled={loading}>
+            {loading ? (
+              <>
+                <ProgressSpinner style={{ width: '24px', height: '18px', marginRight: '8px' }} strokeWidth="4" />
+                Verifying...
+              </>
+            ) : (
+              'Verify Login'
+            )}
+          </button>
+        </form>
+        {message && <p className="verify-login-success-message">{message}</p>}
+        {error && <p className="verify-login-error-message">{error}</p>}
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className="verify-login-container">
-            <div className="verify-login-box">
-                <h1>Enter Otp</h1>
-                <form onSubmit={handleSubmit}>
-                    <div className="verify-login-input-group">
-                        <label>OTP:</label>
-                        <input type="text" value={otp} onChange={(e) => setOtp(e.target.value)} required/>
-                    </div>
-                    <button type="submit">Verify Login</button>
-                </form>
-                {message && <p className="verify-login-sucess-message">{message}</p>}
-                {error && <p className="verify-login-error-message">{error}</p>}
-            </div>
-        </div>
-    )
-}
-
-export default VerifyLogin
+export default VerifyLogin;
